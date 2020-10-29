ecd581d27d3624e5d5adfe6bce532343bfc39fa5
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-10-29 00:05:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -47,6 +47,17 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
+end
+
+
 def current_player(board)
   turn_count(board) % 2 == 0? "X" : "O"
 end
